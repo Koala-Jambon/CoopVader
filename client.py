20b5a9747e241ac35bb413ecf821cff288fdd3d8
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-04-04 18:29:37 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,100 +1,79 @@
-import pyxel
-import os
+import time
 import socket
+import threading
+import os
+from colorama import Fore
 
-class App:
+# Initialisation du serveur sur le port `20101`
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(("", 20101))
+sock.listen()
 
-    def __init__(self, client) -> None:
-        self.client = client
+class ClientClass:
+    def __init__(self, clientValue, clientAdress) -> None:
+        self.clientValue = clientValue
+        self.clientAdress = clientAdress
+        self.currentState = "getNickname"
 
-        self.mainLobbyButton = 0
-        self.userNickname =  ""
-        self.ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.PYXEL_KEY_LETTERS = [pyxel.KEY_A, pyxel.KEY_B, pyxel.KEY_C, pyxel.KEY_D, pyxel.KEY_E, pyxel.KEY_F, pyxel.KEY_G,
-                             pyxel.KEY_H, pyxel.KEY_I, pyxel.KEY_J, pyxel.KEY_K, pyxel.KEY_L, pyxel.KEY_M,
-                             pyxel.KEY_N, pyxel.KEY_O, pyxel.KEY_P, pyxel.KEY_Q, pyxel.KEY_R, pyxel.KEY_S, pyxel.KEY_T,
-                             pyxel.KEY_U, pyxel.KEY_V, pyxel.KEY_W, pyxel.KEY_X, pyxel.KEY_Y, pyxel.KEY_Z]
-        self.currentStage = "getNickname"
-        pyxel.init(228, 128, title="Stars Invader")
-        pyxel.image(0).load(0, 0, './ressources/layer1.png')
-        pyxel.image(1).load(0, 0, './ressources/title.png')
-        pyxel.run(self.update, self.draw)
+        print(Fore.BLUE, f'New Connection : {self.clientAdress}')
 
-    def update(self):
-        status = getattr(self, f'update_{self.currentStage}')()
-        if status != 0:
-            print("An error occured")
-            exit(status)
+        self.handleUser()
 
-    def draw(self):
-        pyxel.cls(0)
-        status = getattr(self, f'draw_{self.currentStage}')()
-        if status != 0:
-            print("An error occured")
-            exit(status)
+    def handleUser(self):
+        while True:
+            #getNickname
+            while self.currentState == "getNickname":
+                try:
+                    userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+                except ConnectionResetError:
+                    print(Fore.RED, f'{self.clientAdress} quit the hard way (ALT+F4)')
+                    self.clientValue.close()
+                    return
+                if len(userMsg) == 2 and userMsg[0] == "sendName" and len(userMsg[1]) <= 12:   
+                    self.clientName = userMsg[1]
+                    print(Fore.GREEN, f'{self.clientAdress} -> {self.clientName}')
+                    self.clientValue.send('continue|Rien à dire...'.encode("utf-8"))
+                    self.currentState = "mainLobby"
+                    break
+                else:
+                    self.clientValue.send('exit|Invalid message'.encode("utf-8"))
+                    self.clientValue.close()
+                    return
+                
+            #mainLobby
+            while self.currentState == "mainLobby":
+                try:
+                    userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+                except ConnectionResetError:
+                    print(Fore.RED, f'{self.clientAdress} / {self.clientName} quit the hard way (ALT+F4)')
+                    self.clientValue.close()
+                    return
 
-    def update_getNickname(self):
-        if pyxel.btnp(pyxel.KEY_BACKSPACE):
-            self.userNickname = self.userNickname[:-1]
-            return 0
-        
-        if len(self.userNickname) >= 12:    return 0
+                if len(userMsg) == 2 and userMsg[0] == 'button' and userMsg[1] == 'quit':
+                    self.clientValue.close()
+                    return
+                elif len(userMsg) == 2 and userMsg[0] == 'button' and userMsg[1] == 'joinLobby':
+                    self.clientValue.send('lobbyList|Rien à dire...'.encode("utf-8"))
+                    break
+                elif len(userMsg) == 2 and userMsg[0] == 'button' and userMsg[1] == 'createLobby':
+                    self.clientValue.send('continue|Rien à dire...'.encode("utf-8"))
+                    break
+                else:
+                    self.clientValue.send('exit|Invalid message'.encode("utf-8"))
+                    self.clientValue.close()
+                    return
 
-        if pyxel.btnp(pyxel.KEY_RETURN):
-            self.client.send(f'sendName|{self.userNickname}'.encode("utf-8"))
-            srvMsg = self.client.recv(1024).decode("utf-8").split('|', 1)
-            if len(srvMsg) != 2 or srvMsg[0] != "continue" or srvMsg[0] == "exit": return 1
-            self.currentStage = "mainLobby"
+if os.name == "posix":
+    os.system("clear")
+else:
+    os.system("cls")
 
-            return 0
-        
-        for i in range(26):
-            if pyxel.btnp(self.PYXEL_KEY_LETTERS[i]):
-                self.userNickname += self.ALPHABET[i]
-                return 0
-        return 0
+print(Fore.RED, "Server Started")
 
-    def draw_getNickname(self):
-        pyxel.blt(30, 5, 1, 5, 0, 167, 25)
-        pyxel.text(88, pyxel.height/2 - 8, "VOTRE PSEUDO:", 13)
-        pyxel.text((pyxel.width - len(self.userNickname)*4 ) / 2, pyxel.height/2, self.userNickname, 7)
-        pyxel.blt(pyxel.width / 2 - 8, pyxel.height - 24, 0, 16, 0, 16, 16)
-        return 0
-
-    def update_mainLobby(self):
-        if pyxel.btnp(pyxel.KEY_RETURN):
-            if self.mainLobbyButton == 0: 
-                self.client.send(f'button|quit'.encode("utf-8"))
-                pyxel.quit()
-            elif self.mainLobbyButton == 1: self.currentStage = "joinLobby"
-            elif self.mainLobbyButton == 2: self.currentStage = "createLobby"
-            self.client.send(f'button|{self.currentStage}'.encode("utf-8"))
-            return 0
-
-        for NAVIGATION_KEY in [pyxel.KEY_UP, pyxel.KEY_DOWN]:
-            if pyxel.btnp(NAVIGATION_KEY):
-                self.mainLobbyButton += [pyxel.KEY_UP, pyxel.KEY_DOWN].index(NAVIGATION_KEY) * 2 - 1
-                break
-        
-        if self.mainLobbyButton == 3: self.mainLobbyButton = 0
-        if self.mainLobbyButton == -1: self.mainLobbyButton = 2
-        return 0
-
-    def draw_mainLobby(self):
-        pyxel.text(0, 0, f'{self.mainLobbyButton}', 7)
-        return 0
-    
-if __name__ == "__main__":
-    if os.name == "posix":
-        os.system("clear")
-    else:
-        os.system("cls")
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while True:
+    newClient, newClientAdress = sock.accept()    
     try:
-        client.connect(("localhost", 20101))
-    except OSError:
-        print("Cannot connect to the server ; Try updating ; Try later")
-        exit()
-
-    App(client)
+        threading.Thread(target=ClientClass, args=(newClient, newClientAdress)).start()
+    except:
+        newClient.close()
