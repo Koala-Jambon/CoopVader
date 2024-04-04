7f07f51faa95ef9b2ddbc3430258b9b3e39c3673
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-04-04 18:02:48 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import time
 import socket
 import threading
-from colorama import Fore
+import os
+from colorama import Fore, Style
 
 # Initialisation du serveur sur le port `20101`
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,7 +15,32 @@ class ClientClass:
         self.clientValue = clientValue
         self.clientAdress = clientAdress
 
-        print(Fore.BLUE, f'Nouveau client : {self.clientAdress}')
+        print(Fore.BLUE, f'New Connection : {self.clientAdress}', Style.RESET_ALL)
+
+        self.getUserNickname()
+
+    def getUserNickname(self):
+        while True:
+            userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+            if len(userMsg) == 2 and userMsg[0] == "sendName" and len(userMsg[1]) <= 12:   
+                self.clientName = userMsg[1]
+                print(Fore.GREEN, f'{self.clientAdress} -> {self.clientName}', Style.RESET_ALL)
+                self.clientValue.send('continue|Rien à dire...'.encode("utf-8"))
+            else:
+                self.clientValue.send('exit|Invalid message'.encode("utf-8"))
+                self.clientValue.close()
+                return
+            
+    def mainLobby(self):
+        while True:
+            pass
+
+if os.name == "posix":
+    os.system("clear")
+else:
+    os.system("cls")
+
+print(Fore.RED, "Server Started", Style.RESET_ALL)
 
 while True:
     newClient, newClientAdress = sock.accept()    
