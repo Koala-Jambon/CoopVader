34ecbb809160e684b1149f1cce0b2f01b7b37681
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-04-07 20:57:51 +0200

    ♏ Réfactorrrrrrre

--- client.py
+++ client.py
@@ -1,202 +1,215 @@
-import pyxel
-import os
+import time
 import socket
+import threading
+import os
+from colorama import Fore
 
-class App:
+# Initialisation du serveur sur le port `20101`
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(("", 20101))
+sock.listen()
 
-    def __init__(self, client) -> None:
-        self.client = client
+bannedIPs = []
+connectionDict = {}
+threadDict = {}
+exitProgramm = False
 
-        self.mY = 0
-        self.mX = 0
-        self.gameInfos = [{}, {}]
+gameInfos = [{}]
+
+partyList = [{"number" : 0, "state" : None, "players" : []}]
+
+class ClientClass:
+    def __init__(self, clientValue, clientAdress) -> None:
         self.gameNumber = 0
-        self.latestJoinButton = -1
-        self.loadedParties = [None, None, None]
-        self.mainLobbyButton = 0
-        self.joinLobbyButton = 0
-        self.userNickname =  ""
-        self.ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.PYXEL_KEY_LETTERS = [pyxel.KEY_A, pyxel.KEY_B, pyxel.KEY_C, pyxel.KEY_D, pyxel.KEY_E, pyxel.KEY_F, pyxel.KEY_G,
-                             pyxel.KEY_H, pyxel.KEY_I, pyxel.KEY_J, pyxel.KEY_K, pyxel.KEY_L, pyxel.KEY_M,
-                             pyxel.KEY_N, pyxel.KEY_O, pyxel.KEY_P, pyxel.KEY_Q, pyxel.KEY_R, pyxel.KEY_S, pyxel.KEY_T,
-                             pyxel.KEY_U, pyxel.KEY_V, pyxel.KEY_W, pyxel.KEY_X, pyxel.KEY_Y, pyxel.KEY_Z]
+        self.clientValue = clientValue
+        self.clientAdress = clientAdress
+        self.playerNumber = 0
         self.currentState = "getNickname"
-        pyxel.init(228, 128, title="Stars Invader")
-        pyxel.image(0).load(0, 0, './ressources/layer1.png')
-        pyxel.image(1).load(0, 0, './ressources/title.png')
-        pyxel.run(self.update, self.draw)
 
-    def update(self):
-        status = getattr(self, f'update_{self.currentState}')()
-        if status != 0:
-            print("An error occured", self.currentState)
-            exit(status)
+        print(Fore.BLUE, f'New connection : {self.clientAdress}')
 
-    def draw(self):
-        pyxel.cls(0)
-        status = getattr(self, f'draw_{self.currentState}')()
-        if status != 0:
-            print("An error occured")
-            exit(status)
+        self.handleUser()
 
-    def update_getNickname(self):
-        if pyxel.btnp(pyxel.KEY_BACKSPACE):
-            self.userNickname = self.userNickname[:-1]
-            return 0
-        
-        if len(self.userNickname) >= 12:    return 0
+    def quit(self):
+        print(Fore.RED, f'Someone quit : {self.clientAdress} - getNickname')
+        self.clientValue.close()
+        exit(1)
 
-        if pyxel.btnp(pyxel.KEY_RETURN):
-            self.client.send(f'sendName|{self.userNickname}'.encode("utf-8"))
-            srvMsg = self.client.recv(1024).decode("utf-8").split('|', 1)
-            if len(srvMsg) != 2 or srvMsg[0] != "continue" or srvMsg[0] == "exit": return 1
-            self.currentState = "mainLobby"
+    def handleUser(self):
+        try:
+            while True:
+                #getNickname
+                while self.currentState == "getNickname":
+                    try: userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+                    except ConnectionResetError: self.quit()
+                    if len(userMsg) == 2 and userMsg[0] == "sendName" and len(userMsg[1]) <= 12:   
+                        self.clientName = userMsg[1]
+                        print(Fore.GREEN, f'{self.clientAdress} -> {self.clientName}')
+                        self.clientValue.send('continue|skip'.encode("utf-8"))
+                        self.currentState = "mainLobby"
+                        break
+                    else: self.quit()
+                    
+                #mainLobby
+                while self.currentState == "mainLobby":
+                    try: userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+                    except ConnectionResetError: self.quit()
 
-            return 0
-        
-        for i in range(26):
-            if pyxel.btnp(self.PYXEL_KEY_LETTERS[i]):
-                self.userNickname += self.ALPHABET[i]
-                return 0
-        return 0
+                    if len(userMsg) != 2 or (userMsg[0] == 'button' and userMsg[1] == 'quit'): self.quit()
+                    elif userMsg[0] == 'button' and userMsg[1] == 'joinLobby':
+                        self.clientValue.send(f'continue|{len(partyList)-1}'.encode("utf-8"))
+                        self.currentState = "joinLobby"
+                        break
+                    elif userMsg[0] == 'button' and userMsg[1] == 'waitGame':
+                        self.clientValue.send('continue|skip'.encode("utf-8"))
+                        self.currentState = "waitGame"
+                        break
+                
+                #joinLobby
+                while self.currentState == "joinLobby":
+                    try: userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+                    except ConnectionResetError: self.quit()
+                    
+                    if len(userMsg) != 2 or userMsg[0] not in ["requestPartyList", "button"]: self.quit()
+                    if userMsg[0] == "requestPartyList":
+                        if int(userMsg[1]) >= len(partyList) - 1: userMsg[1] = len(partyList)-2
+                        if int(userMsg[1]) in [0, 1]: userMsg[1] = 2
+                        else: userMsg[1] = int(userMsg[1])
+                        indexList = [x for x in range(userMsg[1]-1, userMsg[1]+2) if x > 0 and x < len(partyList)] + [0 for x in range(userMsg[1]-1, userMsg[1]+2) if x <= 0 or x >= len(partyList)]
+                        self.clientValue.send(f'sendPartyList|{partyList[indexList[0]]}|{partyList[indexList[1]]}|{partyList[indexList[2]]}|{len(partyList)-1}'.encode("utf-8"))
+                    elif userMsg[0] == "button" and userMsg[1] == "quit": self.currentState = "mainLobby"
+                    elif userMsg[0] == "button" and int(userMsg[1]) < len(partyList):
+                        if len(partyList[int(userMsg[1])]["players"]) == 2: self.clientValue.send(f'continue|refused'.encode("utf-8")) 
+                        else:
+                            partyList[int(userMsg[1])]["players"].append(f'{self.clientAdress}')
+                            if len(partyList[int(userMsg[1])]["players"]) == 2: 
+                                partyList[int(userMsg[1])]["state"] = "FULL"
+                                self.currentState = "inGame"
+                                self.playerNumber = partyList[int(userMsg[1])]["players"].index(f'{self.clientAdress}')
+                                self.clientValue.send(f'continue|playing'.encode("utf-8"))
+                            else: 
+                                partyList[int(userMsg[1])]["state"] = self.clientName
+                                self.gameNumber = userMsg[1]
+                                self.currentState = "waitGame"
+                                self.clientValue.send(f'continue|joined'.encode("utf-8"))
+                    else: self.quit()
 
-    def draw_getNickname(self):
-        pyxel.blt(30, 5, 1, 5, 0, 167, 25)
-        pyxel.text(88, pyxel.height/2 - 8, "VOTRE PSEUDO:", 13)
-        pyxel.text((pyxel.width - len(self.userNickname)*4 ) / 2, pyxel.height/2, self.userNickname, 7)
-        pyxel.blt(pyxel.width / 2 - 8, pyxel.height - 24, 0, 16, 0, 16, 16)
-        return 0
+                while self.currentState == "waitGame":
+                    try: userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+                    except ConnectionResetError: self.quit()
+                    
+                    if len(userMsg) != 2 or userMsg[0] != "waiting" or int(userMsg[1]) >= len(partyList): self.quit()
+                    
+                    if len(partyList[int(userMsg[1])]["players"]) == 2:
+                        self.currentState = "inGame"
+                        self.playerNumber = partyList[int(userMsg[1])]["players"].index(f'{self.clientAdress}')
+                        self.clientValue.send(f'inGame|{self.gameNumber}'.encode("utf-8"))
+                    else: self.clientValue.send(f'wait|{self.gameNumber}'.encode("utf-8"))
+                    
+                while self.currentState == "inGame":
+                    try: userMsg = self.clientValue.recv(1024).decode("utf-8").split('|', 1)
+                    except ConnectionResetError: self.quit()
+                    
+                    if len(userMsg) != 2 or userMsg[0] != "packets": self.quit()
+                    
+                    userMsg = userMsg[1].split("|", 2)
+                    if len(userMsg) != 3: self.quit()
+                    gameInfos[self.playerNumber]["coords"][0] += self.mX
+                    gameInfos[self.playerNumber]["coords"][1] += self.mY
+        except ConnectionAbortedError:
+            print(Fore.RED, f'{self.clientAdress} was kicked')
+            self.clientValue.close()
 
-    def update_mainLobby(self):
-        if pyxel.btnp(pyxel.KEY_RETURN):
-            if self.mainLobbyButton == 0: 
-                self.client.send(f'button|quit'.encode("utf-8"))
-                pyxel.quit()
-            elif self.mainLobbyButton == 1: self.currentState = "joinLobby"
-            elif self.mainLobbyButton == 2: self.currentState = "waitGame"
-            self.client.send(f'button|{self.currentState}'.encode("utf-8"))
-            srvMsg = self.client.recv(1024).decode("utf-8").split('|', 1)
-            if self.currentState == "waitGame" and (len(srvMsg) != 2 or srvMsg[0] != "continue"): return 1
-            if self.currentState == "joinLobby": 
-                if len(srvMsg) != 2 or srvMsg[0] != "continue": return 1
-                self.numberOfParties = int(srvMsg[1])
-                self.latestJoinButton = -1
+def executeAdmin():
+    instruction = {"ban" : f"{Fore.WHITE} MAN BAN - Kicks then bans an IP; example : '$>ban 127.0.0.1'",
+                   "banlist" : f"{Fore.WHITE} MAN BANLIST - Returns the list of banned IPs",
+                   "clear" : f"{Fore.WHITE} MAN CLEAR - Clears the server log shell",
+                   "cls" : f"{Fore.WHITE} MAN CLS - Clears the server command shell",
+                   "echo" : f"{Fore.WHITE} MAN ECHO - Writes anything to the server log shell",
+                   "execas" : f"{Fore.WHITE} MAN EXECAS - ... Empty ... For now!",
+                   "kick" : f"{Fore.WHITE} MAN KICK - Kicks an IP;PORT; example : '$>kick 127.0.0.1;20201'",
+                   "list" : f"{Fore.WHITE} MAN LIST - Returns the list of connected IPs",
+                   "man" : f"{Fore.WHITE} MAN MAN - RTFM",
+                   "pardon" : f"{Fore.WHITE} MAN PARDON - Pardons an IP; example : '$>pardon 127.0.0.1'",
+                   "stop" : f"{Fore.WHITE} MAN STOP - Stops the server..."}
+    commandList = list(instruction.keys())
+    global exitProgramm
+    while True:
+        with open('adminCommand.txt', 'r') as cmdFile:
+            command = cmdFile.readline()
+        if command != "" : 
+            with open('adminCommand.txt', 'w') as file:
+                file.truncate(0)
+            splitedCommand = command.split(" ", 1)
+            if splitedCommand[0] not in commandList:
+                print(Fore.CYAN, f'Command keyword "{splitedCommand[0]}" not reckognised')
+                continue
+            if splitedCommand[0] == "stop":
+                exitProgramm = True
+                exit(0)
+            elif splitedCommand[0] == "clear":
+                if os.name == "posix":
+                    os.system("clear")
+                else:
+                    os.system("cls")
+                print(Fore.RED, "Server Started")
+            elif splitedCommand[0] == "man":
+                if len(splitedCommand) == 1:
+                    print(Fore.CYAN, "Here is the list of available commands :")
+                    for command in commandList: print(f"    -{command}")
+                elif splitedCommand[1] in commandList: print(instruction[splitedCommand[1]])
+                else: print(Fore.CYAN, f'Man keyword "{splitedCommand[1]}" not reckognised')
+            elif splitedCommand[0] == "echo":
+                print(Fore.WHITE, splitedCommand[1])
+            elif splitedCommand[0] == "list":
+                print(Fore.CYAN, "Here is a list of connected IPs :", list(connectionDict.keys()))
+            elif splitedCommand[0] == "kick":
+                if splitedCommand[1] not in list(connectionDict.keys()): 
+                    print(Fore.CYAN, f"'{splitedCommand[1]}' cannot be kicked")
+                    continue
+                connectionDict[f'{splitedCommand[1]}'].close()
+            elif splitedCommand[0] == "ban":
+                if splitedCommand[1] in bannedIPs: continue
+                for key in list(connectionDict.keys()):
+                    if key.split(";", 1)[0] == splitedCommand[1]: connectionDict[f'{key}'].close()
+                bannedIPs.append(splitedCommand[1])
+                print(Fore.BLUE, f'{splitedCommand[1]} was banned')
+            elif splitedCommand[0] == "pardon":
+                if splitedCommand[1] in bannedIPs: 
+                    bannedIPs.remove(splitedCommand[1])
+                    print(Fore.BLUE, f'{splitedCommand[1]} was pardoned')
+                else: print(Fore.BLUE, f'{splitedCommand[1]} is not banned')
+            elif splitedCommand[0] == "banlist":
+                print(Fore.CYAN, "Here is a list of banned IPs :", bannedIPs)
+            elif splitedCommand[0] == "execas":
+                print(Fore.CYAN, "Commin' soon!")
 
-            self.mainLobbyButton = 0
-            return 0
+def main():
+    while True:
+        newClient, newClientAdress = sock.accept()    
+        try:
+            if newClientAdress[0] in bannedIPs: 
+                print(Fore.BLUE, f'{newClientAdress}(banned) tried to reconnect')
+                exit(0)
+            connectionDict[f"{newClientAdress[0]};{newClientAdress[1]}"] = newClient
+            threadDict[f"{newClientAdress[0]};{newClientAdress[1]}"] = threading.Thread(target=ClientClass, args=(newClient, newClientAdress), daemon=True)
+            threadDict[f"{newClientAdress[0]};{newClientAdress[1]}"].start()
+        except:
+            newClient.close()
 
-        for NAVIGATION_KEY in [pyxel.KEY_UP, pyxel.KEY_DOWN]:
-            if pyxel.btnp(NAVIGATION_KEY):
-                self.mainLobbyButton += [pyxel.KEY_UP, pyxel.KEY_DOWN].index(NAVIGATION_KEY) * 2 - 1
-                break
-        
-        if self.mainLobbyButton == 3: self.mainLobbyButton = 0
-        if self.mainLobbyButton == -1: self.mainLobbyButton = 2
-        return 0
+if os.name == "posix":
+    os.system("clear")
+else:
+    os.system("cls")
 
-    def draw_mainLobby(self):
-        pyxel.text(0, 0, f'{self.mainLobbyButton}', 7)
-        pyxel.text(100, 64, f'{["quit", "join", "create"][self.mainLobbyButton]}',7)
-        return 0
-    
-    def update_joinLobby(self):
-        if pyxel.btnp(pyxel.KEY_RETURN):
-            if self.joinLobbyButton == 0: 
-                self.client.send(f'button|quit'.encode("utf-8"))
-                self.currentState = "mainLobby"
-            else:
-                self.client.send(f'button|{self.joinLobbyButton}'.encode("utf-8"))
-                srvMsg = self.client.recv(1024).decode("utf-8").split('|', 1)
-                if srvMsg[0] != "continue": return 1
-                if srvMsg[1] == "refused": return 0
-                if srvMsg[1] == "joined":
-                    self.currentState = "waitGame"
-                    self.gameNumber = self.joinLobbyButton
-                    return 0
-                elif srvMsg[1] == "playing":
-                    self.currentState = "inGame"
-                    self.gameInfos = [{"coords" : [0, 0], "lives" : 3, "bonus" : "None"}, {"coords" : [0, 0], "lives" : 3, "bonus" : "None"}]
-                    self.mY = 0
-                    self.mX = 0
-                    self.gameNumber = self.joinLobbyButton
-                    return 0
+print(Fore.RED, "Server Started")
 
-        for NAVIGATION_KEY in [pyxel.KEY_UP, pyxel.KEY_DOWN]:
-            if pyxel.btnp(NAVIGATION_KEY):
-                self.joinLobbyButton += [pyxel.KEY_UP, pyxel.KEY_DOWN].index(NAVIGATION_KEY) * 2 - 1
-                break
-        
-        if self.joinLobbyButton > self.numberOfParties: self.joinLobbyButton = self.numberOfParties
-        if self.joinLobbyButton < 0: self.joinLobbyButton = 0
-        
-        if self.joinLobbyButton != self.latestJoinButton: 
-            self.client.send(f'requestPartyList|{self.joinLobbyButton}'.encode("utf-8"))
-            srvMsg = self.client.recv(1024).decode("utf-8").split('|', 1)
-            if len(srvMsg) != 2 or srvMsg[0] != 'sendPartyList': return 1
-            self.loadedParties = [eval(x)['state'] for x in srvMsg[1].split('|', 3)[:-1]]
-            self.latestJoinButton = self.joinLobbyButton      
+threading.Thread(target=executeAdmin, daemon=True).start()
+threading.Thread(target=main, daemon=True).start()
 
-        return 0
-    
-    def draw_joinLobby(self):
-        pyxel.text(0, 0, f'{self.joinLobbyButton}', 7)
-        pyxel.text(20, 20, f'{["quit" if self.joinLobbyButton == 0 else "select" for x in range(1)][0]}', 7)
-        pyxel.text(100, 40, f'{self.loadedParties[0]}', 7)
-        pyxel.text(100, 60, f'{self.loadedParties[1]}', 7)
-        pyxel.text(100, 80, f'{self.loadedParties[2]}', 7)
-        return 0
-    
-    def update_waitGame(self):
-        self.client.send(f"waiting|{self.gameNumber}".encode("utf-8"))
-        srvMsg = self.client.recv(1024).decode("utf-8").split('|', 1)
-        print(srvMsg)
-        if len(srvMsg) != 2 or srvMsg[0] not in ["wait", "inGame"]: return 1
-        if srvMsg[0] == "wait": self.gameNumber = int(srvMsg[1])
-        elif srvMsg[0] == "inGame":
-            self.currentState == "inGame"
-            self.gameNumber = int(srvMsg[1])
-            self.gameInfos = [{"coords" : [0, 0], "lives" : 3, "bonus" : "None"}, {"coords" : [0, 0], "lives" : 3, "bonus" : "None"}]
-            self.mY = 0
-            self.mX = 0
+while not exitProgramm:
+    pass
 
-        return 0
-
-    def draw_waitGame(self):
-        pyxel.text(0, 0, "Waiting", 7)
-        return 0
-    
-    def update_inGame(self):
-        if pyxel.btnp(pyxel.KEY_Z): self.mY += 1
-        if pyxel.btnp(pyxel.KEY_S): self.mY -= 1
-        if pyxel.btnp(pyxel.KEY_D): self.mX += 1
-        if pyxel.btnp(pyxel.KEY_Q): self.mX -= 1
-
-        self.action = None
-
-        self.client.send(f'packets|{self.mX}|{self.mY}|{self.action}')
-        #srvMsg = self.client.recv(1024).decode("utf-8").split('|', 1)
-        #if srvMsg[0] not in ["fine"]: return 1
-
-        self.gameInfos[0]["coords"][0] += self.mX
-        self.gameInfos[0]["coords"][1] += self.mY
-        return 0
-
-    def draw_inGame(self):
-        pyxel.rect(self.gameInfos[0]["coords"][0], self.gameInfos[0]["coords"][1], 10, 10, 7)
-        pyxel.rect(self.gameInfos[1]["coords"][0], self.gameInfos[1]["coords"][1], 10, 10, 8)
-
-if __name__ == "__main__":
-    if os.name == "posix":
-        os.system("clear")
-    else:
-        os.system("cls")
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client.connect(("localhost", 20101))
-    except OSError:
-        print("Cannot connect to the server ; Try updating ; Try later")
-        exit()
-
-    App(client)
+print(Fore.RED, "Server stopped", Fore.RESET)
+exit(0)
