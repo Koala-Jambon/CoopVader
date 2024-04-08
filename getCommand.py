71e5fb0f608fcbb3e52c78cf8c85cfd4ccc3e28a
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-04-08 18:04:52 +0200

    Refactor du truc le plus useless

--- getCommand.py
+++ getCommand.py
@@ -1,22 +1,16 @@
 from os import system, name
 from colorama import Fore
 
-
-if name == "posix":
-    system("clear")
-else:
-    system("cls")
+if name == "posix": system("clear")
+else: system("cls")
 
 print(Fore.WHITE, "Welcome to shell-invader!")
 
 while True:
     commandInput = input("$>")
     if commandInput == "cls":
-        if name == "posix":
-            system("clear")
-        else:
-            system("cls")
+        if name == "posix": system("clear")
+        else: system("cls")
         print("Welcome to shell-invader!")
         continue
-    with open('adminCommand.txt', 'w') as file:
-        file.write(commandInput)
+    with open('adminCommand.txt', 'w') as file: file.write(commandInput)
