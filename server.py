36f78e6f7417f869f46add6ddde62cbacb841ad1
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-04-08 16:20:10 +0200

    Riz facteur

--- server.py
+++ server.py
@@ -122,66 +122,59 @@ class ClientClass:
             self.clientValue.close()
 
 def executeAdmin():
-    instruction = {"ban" : f"{Fore.WHITE} MAN BAN - Kicks then bans an IP; example : '$>ban 127.0.0.1'",
-                   "banlist" : f"{Fore.WHITE} MAN BANLIST - Returns the list of banned IPs",
-                   "clear" : f"{Fore.WHITE} MAN CLEAR - Clears the server log shell",
-                   "cls" : f"{Fore.WHITE} MAN CLS - Clears the server command shell",
-                   "echo" : f"{Fore.WHITE} MAN ECHO - Writes anything to the server log shell",
-                   "execas" : f"{Fore.WHITE} MAN EXECAS - ... Empty ... For now!",
-                   "kick" : f"{Fore.WHITE} MAN KICK - Kicks an IP;PORT; example : '$>kick 127.0.0.1;20201'",
-                   "list" : f"{Fore.WHITE} MAN LIST - Returns the list of connected IPs",
-                   "man" : f"{Fore.WHITE} MAN MAN - RTFM",
-                   "pardon" : f"{Fore.WHITE} MAN PARDON - Pardons an IP; example : '$>pardon 127.0.0.1'",
-                   "stop" : f"{Fore.WHITE} MAN STOP - Stops the server..."}
-    commandList = list(instruction.keys())
     global exitProgramm
+    instruction = {"ban"     : f"{Fore.WHITE} MAN BAN - Kicks then bans an IP; example : '$>ban 127.0.0.1'",
+                   "banlist" : f"{Fore.WHITE} MAN BANLIST - Returns the list of banned IPs",
+                   "clear"   : f"{Fore.WHITE} MAN CLEAR - Clears the server log shell",
+                   "cls"     : f"{Fore.WHITE} MAN CLS - Clears the server command shell",
+                   "echo"    : f"{Fore.WHITE} MAN ECHO - Writes anything to the server log shell",
+                   "execas"  : f"{Fore.WHITE} MAN EXECAS - ... Empty ... For now!",
+                   "kick"    : f"{Fore.WHITE} MAN KICK - Kicks an IP;PORT; example : '$>kick 127.0.0.1;20201'",
+                   "list"    : f"{Fore.WHITE} MAN LIST - Returns the list of connected IPs",
+                   "man"     : f"{Fore.WHITE} MAN MAN - RTFM",
+                   "pardon"  : f"{Fore.WHITE} MAN PARDON - Pardons an IP; example : '$>pardon 127.0.0.1'",
+                   "stop"    : f"{Fore.WHITE} MAN STOP - Stops the server..."}
+    commandList = list(instruction.keys())
     while True:
         with open('adminCommand.txt', 'r') as cmdFile:
             command = cmdFile.readline()
-        if command != "" : 
-            with open('adminCommand.txt', 'w') as file:
-                file.truncate(0)
-            splitedCommand = command.split(" ", 1)
-            if splitedCommand[0] not in commandList:
-                print(Fore.CYAN, f'Command keyword "{splitedCommand[0]}" not reckognised')
+        if command == "": continue
+        with open('adminCommand.txt', 'w') as file: file.truncate(0)
+        splitedCommand = command.split(" ", 1)
+        if splitedCommand[0] not in commandList: print(Fore.CYAN, f'Command keyword "{splitedCommand[0]}" not reckognised')
+        elif splitedCommand[0] == "stop":
+            exitProgramm = True
+            exit(0)
+        elif splitedCommand[0] == "clear":
+            if os.name == "posix": os.system("clear")
+            else: os.system("cls")
+            print(Fore.RED, "Server Started")
+        elif splitedCommand[0] == "man":
+            if len(splitedCommand) == 1:
+                print(Fore.CYAN, "Here is the list of available commands :")
+                for command in commandList: print(f"    -{command}")
+            elif splitedCommand[1] in commandList: print(instruction[splitedCommand[1]])
+            else: print(Fore.CYAN, f'Man keyword "{splitedCommand[1]}" not reckognised')
+        elif splitedCommand[0] == "echo": print(Fore.WHITE, splitedCommand[1])
+        elif splitedCommand[0] == "list": print(Fore.CYAN, "Here is a list of connected IPs :", list(connectionDict.keys()))
+        elif splitedCommand[0] == "kick":
+            if splitedCommand[1] not in list(connectionDict.keys()): 
+                print(Fore.CYAN, f"'{splitedCommand[1]}' cannot be kicked")
                 continue
-            if splitedCommand[0] == "stop":
-                exitProgramm = True
-                exit(0)
-            elif splitedCommand[0] == "clear":
-                if os.name == "posix": os.system("clear")
-                else: os.system("cls")
-                print(Fore.RED, "Server Started")
-            elif splitedCommand[0] == "man":
-                if len(splitedCommand) == 1:
-                    print(Fore.CYAN, "Here is the list of available commands :")
-                    for command in commandList: print(f"    -{command}")
-                elif splitedCommand[1] in commandList: print(instruction[splitedCommand[1]])
-                else: print(Fore.CYAN, f'Man keyword "{splitedCommand[1]}" not reckognised')
-            elif splitedCommand[0] == "echo":
-                print(Fore.WHITE, splitedCommand[1])
-            elif splitedCommand[0] == "list":
-                print(Fore.CYAN, "Here is a list of connected IPs :", list(connectionDict.keys()))
-            elif splitedCommand[0] == "kick":
-                if splitedCommand[1] not in list(connectionDict.keys()): 
-                    print(Fore.CYAN, f"'{splitedCommand[1]}' cannot be kicked")
-                    continue
-                connectionDict[f'{splitedCommand[1]}'].close()
-            elif splitedCommand[0] == "ban":
-                if splitedCommand[1] in bannedIPs: continue
-                for key in list(connectionDict.keys()):
-                    if key.split(";", 1)[0] == splitedCommand[1]: connectionDict[f'{key}'].close()
-                bannedIPs.append(splitedCommand[1])
-                print(Fore.BLUE, f'{splitedCommand[1]} was banned')
-            elif splitedCommand[0] == "pardon":
-                if splitedCommand[1] in bannedIPs: 
-                    bannedIPs.remove(splitedCommand[1])
-                    print(Fore.BLUE, f'{splitedCommand[1]} was pardoned')
-                else: print(Fore.BLUE, f'{splitedCommand[1]} is not banned')
-            elif splitedCommand[0] == "banlist":
-                print(Fore.CYAN, "Here is a list of banned IPs :", bannedIPs)
-            elif splitedCommand[0] == "execas":
-                print(Fore.CYAN, "Commin' soon!")
+            connectionDict[f'{splitedCommand[1]}'].close()
+        elif splitedCommand[0] == "ban":
+            if splitedCommand[1] in bannedIPs: continue
+            for key in list(connectionDict.keys()):
+                if key.split(";", 1)[0] == splitedCommand[1]: connectionDict[f'{key}'].close()
+            bannedIPs.append(splitedCommand[1])
+            print(Fore.BLUE, f'{splitedCommand[1]} was banned')
+        elif splitedCommand[0] == "pardon":
+            if splitedCommand[1] in bannedIPs: 
+                bannedIPs.remove(splitedCommand[1])
+                print(Fore.BLUE, f'{splitedCommand[1]} was pardoned')
+            else: print(Fore.BLUE, f'{splitedCommand[1]} is not banned')
+        elif splitedCommand[0] == "banlist": print(Fore.CYAN, "Here is a list of banned IPs :", bannedIPs)
+        elif splitedCommand[0] == "execas": print(Fore.CYAN, "Commin' soon!")
 
 def main():
     while True:
@@ -193,8 +186,7 @@ def main():
             connectionDict[f"{newClientAdress[0]};{newClientAdress[1]}"] = newClient
             threadDict[f"{newClientAdress[0]};{newClientAdress[1]}"] = threading.Thread(target=ClientClass, args=(newClient, newClientAdress), daemon=True)
             threadDict[f"{newClientAdress[0]};{newClientAdress[1]}"].start()
-        except:
-            newClient.close()
+        except: newClient.close()
 
 if os.name == "posix": os.system("clear")
 else: os.system("cls")
